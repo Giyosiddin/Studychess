f4f8e4d9ed85cb2b3fe51c9b011b109e8eec49b8
Giyosiddin <m_master96@mail.ru>
2021-06-02 05:16:30 +0500

    Update front.blade.php

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -191,6 +191,8 @@
 <script src="{{ asset('/js/jquery.min.js') }}"></script>
 <script src="{{ asset('/js/jquery.selectric.min.js') }}"></script>
 <script src="{{ asset('/js/swiper.min.js') }}"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.5.1/js/swiper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 <script src="{{ asset('/js/bootstrap.min.js') }}"></script>
 <script src="{{ asset('/js/jquery.fancybox.min.js') }}"></script>
 <script src="{{ asset('/js/main.js') }}"></script>
