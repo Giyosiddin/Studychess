ebd9c16bd4f03ec96981fbeee426a9447b51b5f8
Giyosiddin <m_master96@mail.ru>
2021-06-02 08:34:37 +0500

    Update front.blade.php

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -6,13 +6,13 @@
   <!-- <link rel="shortcut icon" href="favicon.ico" type="image/x-icon"> -->
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="{{ asset('font/stylesheet.css') }}">
-  <link rel="stylesheet" href="{{ asset('css/jquery.fancybox.min.css') }}">
-  <link rel="stylesheet" href="{{ asset('css/selectric.css') }}">
-  <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
-  <link rel="stylesheet" href="{{ asset('css/swiper.min.css') }}">
-  <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-  <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
+  <link rel="stylesheet" href="{{ asset('/font/stylesheet.css') }}">
+  <link rel="stylesheet" href="{{ asset('/css/jquery.fancybox.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('/css/selectric.css') }}">
+  <link rel="stylesheet" href="{{ asset('/css/bootstrap.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('/css/swiper.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('/css/style.css') }}">
+  <link rel="stylesheet" href="{{ asset('/css/custom.css') }}">
   <title>Study-Chess</title>
 </head>
 
@@ -191,8 +191,6 @@
 <script src="{{ asset('/js/jquery.min.js') }}"></script>
 <script src="{{ asset('/js/jquery.selectric.min.js') }}"></script>
 <script src="{{ asset('/js/swiper.min.js') }}"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.5.1/js/swiper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 <script src="{{ asset('/js/bootstrap.min.js') }}"></script>
 <script src="{{ asset('/js/jquery.fancybox.min.js') }}"></script>
 <script src="{{ asset('/js/main.js') }}"></script>
