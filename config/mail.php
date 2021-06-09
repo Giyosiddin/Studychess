6dd41baf0ba0591b300661c29a4d3200b8603013
Giyosiddin <m_master96@mail.ru>
2021-06-09 22:15:34 +0500

    Update mail.php

--- config/mail.php
+++ config/mail.php
@@ -61,6 +61,14 @@ return [
             'transport' => 'array',
         ],
     ],
+    
+    'stream' => [
+        'ssl' => [
+            'allow_self_signed' => true,
+            'verify_peer'       => false,
+            'verify_peer_name'  => false,
+        ],
+    ],
 
     /*
     |--------------------------------------------------------------------------
